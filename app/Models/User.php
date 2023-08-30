9670c5a402f9e66d0100c8b2bb9ff4afe5bcba16
Inderjit <inder.js2007@gmail.com>
2023-08-30 10:29:38 -0230

    updated user model added relations and functions of current badge and unlocked achivements

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Lesson;
+use App\Models\Comment;
 
 class User extends Authenticatable
 {
@@ -42,4 +44,51 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function watched()
+    {
+        return $this->belongsToMany(Lesson::class, 'user_lessons')->wherePivot('watched', true);;
+    }
+
+    public function unlockedAchievementsCount()
+    {
+        $lessonsWatched = $this->watched()->count();
+        $commentsWritten = $this->comments()->count();
+
+        $achievements = [
+            'First Lesson Watched' => $lessonsWatched >= 1,
+            '5 Lessons Watched' => $lessonsWatched >= 5,
+            '10 Lessons Watched' => $lessonsWatched >= 10,
+            '25 Lessons Watched' => $lessonsWatched >= 25,
+            '50 Lessons Watched' => $lessonsWatched >= 50,
+            'First Comment Written' => $commentsWritten >= 1,
+            '3 Comments Written' => $commentsWritten >= 3,
+            '5 Comments Written' => $commentsWritten >= 5,
+            '10 Comment Written' => $commentsWritten >= 10,
+            '20 Comment Written' => $commentsWritten >= 20,
+        ];
+
+        return count(array_filter($achievements));
+    }
+
+    public function currentBadge()
+    {
+        $unlockedAchievementsCount = $this->unlockedAchievementsCount();
+
+        if ($unlockedAchievementsCount >= 10) {
+            return 'Master';
+        } elseif ($unlockedAchievementsCount >= 8) {
+            return 'Advanced';
+        } elseif ($unlockedAchievementsCount >= 4) {
+            return 'Intermediate';
+        } else {
+            return 'Beginner';
+        }
+    }
+
 }
